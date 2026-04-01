ee8112db3600489f4f2d694d5c56f8925c7920b1
Sneha Meshram <snehameshram1710@gmail.com>
2026-04-01 20:21:50 +0100

    Adding Req file

--- MyQuiz/settings.py
+++ MyQuiz/settings.py
@@ -119,8 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static'] 
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 AUTH_USER_MODEL = 'accounts.User'
